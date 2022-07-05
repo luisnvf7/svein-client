b679b2933fafcc353615987fbec7be8b2fddeb86
luisbrick <luis.villalobos@brickcontrol.com>
2022-07-05 07:55:25 -0400

    Setup of the project

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ['./src'],
+          alias: {
+            "@components": "./src/components",
+            "@interfaces": "./src/interfaces",
+            "@styles": "./src/styles",
+            "@utils": "./src/utils"
+          },
+        },
+      ],
+    ],
   };
 };
